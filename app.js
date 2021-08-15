62e6dd404e4581d138dc278fe29aa0a97bb957b6
hachad-14 <hachadsaad14@gmail.com>
2021-08-15 16:27:12 +0100

    test

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ $(document).ready(function() {
     console.log("test");
     document.getElementById("main-page").style.visibility = "hidden";
     document.getElementById("ayou_gram").style.visibility = "visible";
-    document.getElementById("ayou_gram").style.transition = "0.5s ease";
-    document.getElementById("ayou_gram").style.opacity = "1";
     document.body.style.backgroundColor = "#fff"; 
     
     $("#ag_arrow").click(function() {
       document.getElementById("main-page").style.visibility = "visible";
       document.getElementById("ayou_gram").style.visibility = "hidden";
-      document.getElementById("ayou_gram").style.transition = "0s ease";
-      document.getElementById("ayou_gram").style.opacity = "0";
       document.body.style.backgroundColor = "#04003e"; 
     });
   });
