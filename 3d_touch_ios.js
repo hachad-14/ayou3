848d18ebd05820eb157a68075724af64df31e6a1
hachad-14 <hachadsaad14@gmail.com>
2021-08-15 01:27:38 +0100

    test

--- 3d_touch_ios.js
+++ 3d_touch_ios.js
@@ -1,27 +1,8 @@
 document.querySelectorAll('img').forEach(el=> {
-
-    el.addEventListener('dragstart', e => {
-        e.preventDefault(); 
-        return false; 
-    });
-  
-    // customize your pressure force
-    const force = 0.2; 
-  
-    // iOS
     el.addEventListener('touchforcechange', e => {
         if (e.changedTouches[0].force < force) {
             e.preventDefault();
             return false;
         }
     });
-  
-    // Standard, Firefox
-    el.addEventListener('touchstart', e => {
-        if (e.targetTouches[0].force < force) {
-            e.preventDefault();
-            return false;
-        }
-    });
-  
   });
