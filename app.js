915746354fca2ce2d11fe13b060d43c97ba6dd95
hachad-14 <hachadsaad14@gmail.com>
2021-08-15 01:13:05 +0100

    test

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  $("#insta").click(function() {
+    console.log("test");
+    document.getElementById("main-page").style.visibility = "hidden";
+    document.getElementById("ayou_gram").style.visibility = "visible";
+
+    //CODE
+  });
+});
