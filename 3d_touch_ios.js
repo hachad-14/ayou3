ab0ac8f7599ac3aa8d33fde02bd42d2e625d8e69
hachad-14 <hachadsaad14@gmail.com>
2021-08-15 01:28:13 +0100

    test

--- 3d_touch_ios.js
+++ 3d_touch_ios.js
@@ -1,8 +1,14 @@
 document.querySelectorAll('img').forEach(el=> {
+
+    // customize your pressure force
+    const force = 0.2; 
+  
+    // iOS
     el.addEventListener('touchforcechange', e => {
         if (e.changedTouches[0].force < force) {
             e.preventDefault();
             return false;
         }
     });
+  
   });
