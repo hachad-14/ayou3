d6d1fa0d37b633322ccbbde8654e30f00715a014
hachad-14 <hachadsaad14@gmail.com>
2021-08-18 23:07:12 +0100

    test

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
-  window.onload = function(){
-    document.getElementById("main-page").style.visibility = "visible"; 
-    console.log("page loaded.");
-  };
-
+  
   $("#insta").click(function() {
     console.log("test");
     document.getElementById("main-page").style.visibility = "hidden";
     document.getElementById("ayou_gram").style.visibility = "visible";
-    document.body.style.backgroundColor = "#fff"; 
+    document.body.style.backgroundColor = "#fff";
+    document.body.style.position = "absolute"; 
     
     $("#ag_arrow").click(function() {
       document.getElementById("main-page").style.visibility = "visible";
       document.getElementById("ayou_gram").style.visibility = "hidden";
-      document.body.style.backgroundColor = "#04003e"; 
+      document.body.style.backgroundColor = "#04003e";
+      document.body.style.position = "fixed"; 
     });
   });
 });
---
+++ stories.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#s1").click(function() {
+        document.getElementById("s1s").style.visibility = "visible";
+        console.log("test")
+    });
+}); 
