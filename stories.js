907449be336212ebc284ff60264fea5c702bdf27
hachad-14 <hachadsaad14@gmail.com>
2021-08-20 15:11:13 +0100

    test

--- stories.js
+++ stories.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
         document.getElementById("s1s").style.visibility = "visible";
         console.log("test")
     });
+    $("#heart").dblclick(function() {
+        document.getElementById("heart").style.filter = "invert(50%)";
+      });
 }); 
